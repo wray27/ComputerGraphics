1496cce3a5ab38bebf11dd32ab8590ced7a1c7e1
wray27 <aw16997@my.bristol.ac.uk>
2019-02-12 16:06:25 +0000

    first Commit

---
+++ raytracer/Source/skeleton.cpp
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <SDL.h>
+#include "SDLauxiliary.h"
+#include "TestModelH.h"
+#include <stdint.h>
+#include "limits"
+
+using namespace std;
+using glm::vec3;
+using glm::mat3;
+using glm::vec4;
+using glm::mat4;
+
+SDL_Event event;
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 256
+#define FULLSCREEN_MODE false
+
+struct Intersection
+{
+  vec4 position;
+  float distance;
+  int triangleIndex;
+};
+
+float focalLength = 180;
+vec4 cameraPos(0,0.2,-2,1.0);
+
+
+
+/* ----------------------------------------------------------------------------*/
+/* FUNCTIONS                                                                   */
+
+bool Update();
+void Draw(screen* screen);
+bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection );
+//void LoadTestModel( std::vector<Triangle>&triangles );
+
+vector<Triangle> triangles;
+
+
+//void ray_direction(int x, int y);
+
+int main( int argc, char* argv[] )
+{
+  
+  screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+  LoadTestModel(triangles);
+
+  while ( Update())
+    {
+      Draw(screen);
+      SDL_Renderframe(screen);
+    }
+
+  SDL_SaveImage( screen, "screenshot.bmp" );
+
+  KillSDL(screen);
+  return 0;
+}
+
+/*Place your drawing here*/
+void Draw(screen* screen)
+{
+
+  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
+
+  Intersection closestIntersection;
+
+  for(int y = 0; y < SCREEN_HEIGHT; y++) {
+
+    for(int x = 0; x < SCREEN_WIDTH; x++) {
+      vec4 d = vec4(x - (SCREEN_WIDTH / 2), y - (SCREEN_HEIGHT / 2), focalLength, 1.0);
+      bool b = ClosestIntersection(cameraPos,d, triangles, closestIntersection);
+      if(b) {
+        int i = closestIntersection.triangleIndex;
+        vec3 colour = triangles[i].color;
+        PutPixelSDL(screen, x, y, colour);
+      } else {
+        PutPixelSDL(screen, x, y, vec3(1.0,1.0,1.0));
+      }
+    }
+  }
+}
+
+/*Place updates of parameters here*/
+bool Update()
+{
+  static int t = SDL_GetTicks();
+  /* Compute frame time */
+  int t2 = SDL_GetTicks();
+  // float dt = float(t2-t);
+  t = t2;
+
+  SDL_Event e;
+  while(SDL_PollEvent(&e))
+    {
+      if (e.type == SDL_QUIT)
+	{
+	  return false;
+	}
+      else
+	if (e.type == SDL_KEYDOWN)
+	  {
+	    int key_code = e.key.keysym.sym;
+	    switch(key_code)
+	      {
+	      case SDLK_UP:
+		/* Move camera forward */
+		break;
+	      case SDLK_DOWN:
+		/* Move camera backwards */
+		break;
+	      case SDLK_LEFT:
+		/* Move camera left */
+		break;
+	      case SDLK_RIGHT:
+		/* Move camera right */
+		break;
+	      case SDLK_ESCAPE:
+		/* Move camera quit */
+		return false;
+	      }
+	  }  
+    }
+  return true;
+}
+
+
+
+bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection ) {
+
+  bool intersects_triangle = false;
+  closestIntersection.distance = std :: numeric_limits<float>::max();
+
+  for(int i = 0; i < triangles.size(); i++) {
+
+   Triangle triangle = triangles[i];
+   vec4 v0 = triangle.v0;
+   vec4 v1 = triangle.v1;
+   vec4 v2 = triangle.v2;
+   vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
+   vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
+   vec3 b = vec3(start.x-v0.x,start.y-v0.y,start.z-v0.z);
+   vec3 d = vec3(dir.x, dir.y,dir.z);
+
+   mat3 A(-d, e1, e2);
+   vec3 x = glm::inverse( A ) * b;
+
+   float t = x.x;
+   float u = x.y;
+   float v = x.z;
+
+   //vec3 dist = t * d;
+   //float mag = sqrt(dist.length());
+
+
+   if(t >= 0 && u >= 0 && v >= 0 && (u+v) <= 1) {
+    intersects_triangle = true;
+
+    //updating closest intersection
+    if(t < closestIntersection.distance) {
+      closestIntersection.position = start + (t*dir);
+      closestIntersection.triangleIndex = i;
+      closestIntersection.distance = t;
+    }
+
+   }
+  }
+  return intersects_triangle;
+}
+
+mat3 cramersRule(mat3 A) {
+
+  float a = A[0][0];
+  float b = A[0][1];
+  float c = A[0][2];
+  float d = A[1][0];
+  float e = A[1][1];
+  float f = A[1][2];
+  float g = A[2][0];
+  float h = A[2][1];
+  float i = A[2][2];
+
+
+  float det = ((a * e * i) + (b * f * g) +(c * d * h)) - ((b * d * i) + (h * f * a) + (g * e * c));
+  // A = glm::transpose(glm::adjoint(A));
+
+  return (1/det) * A; 
+
+}
+
+// void adjoint(mat3 A){
+
+//   mat3 
+
+// }
---
+++ starfield/Source/skeleton.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <SDL.h>
+#include "SDLauxiliary.h"
+#include "TestModel.h"
+#include <stdint.h>
+
+using namespace std;
+using glm::vec3;
+using glm::mat3;
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 256
+#define FULLSCREEN_MODE false
+
+
+/* ----------------------------------------------------------------------------*/
+/* GLOBAL VARIABLES                                                            */
+int t;
+vector<vec3> stars(1000);
+
+/* ----------------------------------------------------------------------------*/
+/* FUNCTIONS                                                                   */
+
+void Update();
+void ColourScreen(screen* screen);
+void Draw(screen* screen);
+void Interpolate(float a, float b, vector<float>& result);
+void Interpolate2(vec3 a, vec3 b, vector<vec3>& result);
+
+int main( int argc, char* argv[] )
+{
+
+  
+  // float randomNumber;
+
+  for(int i =0; i < stars.size();i++){
+    stars[i].x = float (rand())/float(RAND_MAX/2) -1;
+    stars[i].y = float (rand())/float(RAND_MAX/2) -1;
+    stars[i].z = float (rand())/float(RAND_MAX);
+
+
+  }
+  // for (int i = 0; i < stars.size(); ++i)
+  // {
+  //   cout << "("
+  //   << stars[i].x << "," 
+  //   << stars[i].y << "," 
+  //   << stars[i].z << ")\n"; //prints the result
+  // }
+  
+  
+  screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+  t = SDL_GetTicks();	/*Set start value for timer.*/
+  
+  while( NoQuitMessageSDL() )
+    {
+      Draw(screen);
+      Update();
+      // break;
+      SDL_Renderframe(screen);
+    }
+
+  SDL_SaveImage( screen, "screenshot.bmp" );
+
+  KillSDL(screen);
+  return 0;
+}
+void Interpolate(float a, float b, vector<float>& result)
+{//Most basic version
+  for (int i = a; i <= b; ++i)
+  {
+    result[i-a] = i;
+    
+  }
+}
+void Interpolate2(vec3 a, vec3 b, vector<vec3>& result)
+{//interpolation with colours 
+  float xIncrement = ( b.x - a.x )/(result.size()-1);
+  float yIncrement = ( b.y - a.y )/(result.size()-1);
+  float zIncrement = ( b.z - a.z )/(result.size()-1);
+  for (int i = 0; i <result.size(); ++i)
+  {
+    result[i].x = a.x + i*xIncrement;
+    result[i].y = a.y + i*yIncrement;
+    result[i].z = a.z + i*zIncrement;
+
+  }
+}
+void ColourScreen(screen* screen){
+  vec3 red(1.0,0.0,0.0); //top left
+  vec3 blue(0.0,0.0,1.0); //top right
+  vec3 green(0.0,1.0,0.0); //bottom right
+  vec3 yellow(1.0,1.0,0.0); //bottom left
+  vec3 magenta(1.0,0.1,0.8);
+  vec3 cyan(0.0,0.8,1.0);
+  // vec3 colours[6] = {red,blue,green,yellow,magenta,cyan};
+
+  vector<vec3> leftSide(SCREEN_HEIGHT);
+  vector<vec3> rightSide(SCREEN_HEIGHT);
+
+  Interpolate2(red,yellow,leftSide);
+  Interpolate2(blue,green,rightSide);
+  
+  // uint32_t x;
+  // uint32_t y;
+
+  for(int i=0; i<SCREEN_HEIGHT; i++)
+  {
+    vector<vec3> row(SCREEN_WIDTH);
+    Interpolate2(leftSide[i],rightSide[i],row);
+    for (int j = 0; j < SCREEN_WIDTH; ++j)
+    { 
+
+      PutPixelSDL(screen, j, i, row[j]);    
+    }    
+  }
+
+}
+
+/*Place your drawing here*/
+void Draw(screen* screen)
+{
+  /* Clear buffer */
+  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
+  float u[1000];
+  float v[1000];
+  
+
+  // focal lenght of the camera
+  float f = SCREEN_HEIGHT/2;
+  for(size_t s =0; s < stars.size();s++){
+    vec3 star= 0.2f * vec3(1,1,1)/(stars[s].z*stars[s].z);
+    u[s]= f * (stars[s].x/stars[s].z) +(SCREEN_WIDTH/2);
+    v[s]= f * (stars[s].y/stars[s].z) +(SCREEN_HEIGHT/2);
+    PutPixelSDL(screen, u[s], v[s], star);
+
+  }
+  
+
+  
+
+
+
+}
+
+/*Place updates of parameters here*/
+void Update()
+{
+  /* Compute frame time */
+  static int t = SDL_GetTicks();
+  int t2 = SDL_GetTicks();
+  float dt = float(t2-t);
+  t = t2;
+  float velocity =0.00002;
+  for(size_t s =0; s < stars.size();s++){
+    stars[s].z = stars[s].z - velocity*dt;
+    if(stars[s].z<= 0){
+      stars[s].z +=1;
+    }
+    if(stars[s].z >1){
+      stars[s].z -=1;
+    }
+
+  }
+
+  
+  cout << ".";
+  /*Good idea to remove this*/
+  // std::cout << "Render time: " << dt << " ms." << std::endl;
+  /* Update variables*/
+
+
+
+
+  // vector<vec3> result(4);
+  // vec3 a(1,4,9.2);
+  // vec3 b(4,1,9.8);
+
+  // Interpolate2(a,b,result);
+  // for (int i = 0; i < result.size(); ++i)
+  // {
+  //   cout << "("
+  //   << result[i].x << "," 
+  //   << result[i].y << "," 
+  //   << result[i].z << ")";
+  // }
+
+  // vector<float> result(10);
+  // Interpolate(5,14,result);
+  // for (int i = 0; i < result.size(); ++i)
+  // {
+  //   cout << result[i] << " "; //prints the result
+  // }
+
+
+
+
+}
+
